c89d6405ff50886466857b1a02d1cc5f89d2f60e
mirnes114 <mirnes.husic@bulme.at>
2025-10-09 09:07:21 +0200

    Code für Button und Clockauswahl

--- Software/OLED_Clockswitch/src/main.cpp
+++ Software/OLED_Clockswitch/src/main.cpp
@@ -1,25 +1,57 @@
-#include <U8g2lib.h>
 #include <Arduino.h>
+#include <U8g2lib.h>
 
-// OLED Display starten mit I2C
-U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
+#define ClockButtonSwitch 12
+
+#define LED1 32
+#define LED2 27
+#define LED3 26
+
+
+
+int counter = 0;
+String ClockStates[3] = {"MIDI", "Intern", "Analog"}; //analog, intern, midi clocks
+
 
 void setup() {
-  u8g2.begin();               // Display starten
-  u8g2.clearBuffer();         // Puffer leeren
-  u8g2.setFont(u8g2_font_ncenB14_tr); // Schriftart setzen
-  Serial.begin(9600);         // optional zum Debuggen
+  pinMode(ClockButtonSwitch, INPUT);
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+      
+  Serial.begin(9600);
+}
+
+void ClockCount(int counter) {
+  if(counter % 3 == 0) {
+    Serial.println("Selected Clock: " + ClockStates[0]);
+    digitalWrite(LED1, HIGH);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, LOW);
+  }
+  else if(counter % 3 == 1) {
+    Serial.println("Selected Clock: " + ClockStates[1]);
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, HIGH);
+    digitalWrite(LED3, LOW);
+  }
+  else if(counter % 3 == 2) {
+    Serial.println("Selected Clock: " + ClockStates[2]);
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, HIGH);
+  }
+  else {
+    Serial.println("ClockCount Error");
+  }
 }
 
 void loop() {
-  int state = 0; // Hier später deinen Wert einfügen (z.B. Schalterzustand)
-
-  // Beispiel: Text auf Display
-  u8g2.clearBuffer();          // Puffer leeren
-  u8g2.setCursor(0, 30);       // Textposition
-  u8g2.print("State: ");
-  u8g2.print(state);
-  u8g2.sendBuffer();           // Puffer auf Display übertragen
-
-  delay(100);                  // kleines Delay
+  if(digitalRead(ClockButtonSwitch) == HIGH){
+    ClockCount(counter);
+    counter++;
+    while(digitalRead(ClockButtonSwitch) == HIGH);
+  }
+  
 }
+
