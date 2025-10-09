83c5eab5895e3988fba270946d2da67a7241cdca
Carmelo Miorini <carmelo.miorini@bulme.at>
2025-10-09 09:49:09 +0200

    Preset Pages + und -button

--- Software/oled_presets/src/main.cpp
+++ Software/oled_presets/src/main.cpp
@@ -5,6 +5,9 @@
 U8G2_SSD1306_128X32_UNIVISION_F_SW_I2C u8g2(U8G2_R0, 19, 18, U8X8_PIN_NONE);
 
 const int BUTTON_PIN = 22;
+
+const int BUTTON2_PIN = 23;
+
 int presetNumber = 0;
 
 void setup(void) {
@@ -23,11 +26,24 @@ void loop(void) {
 
     char Ausgabe[20];
 
-    sprintf(Ausgabe, "Preset %d", presetNumber);
+    sprintf(Ausgabe, "Preset Page %d", presetNumber);
 
     u8g2.clearBuffer();
     u8g2.drawStr(12, 25, Ausgabe);
     u8g2.sendBuffer();
     while(digitalRead(BUTTON_PIN));
+  }
+  if(digitalRead(BUTTON2_PIN)&& presetNumber > 0){
+    presetNumber--;
+
+    char Ausgabe[20];
+
+    sprintf(Ausgabe, "Preset Page %d", presetNumber);
+
+    u8g2.clearBuffer();
+    u8g2.drawStr(12, 25, Ausgabe);
+    u8g2.sendBuffer();
+    while(digitalRead(BUTTON2_PIN));
+
   }
 }	
