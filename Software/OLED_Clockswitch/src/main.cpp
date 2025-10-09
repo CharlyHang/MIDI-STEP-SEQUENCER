2a16bd474b223bbd726b62d75a197b054956936d
mirnes114 <mirnes.husic@bulme.at>
2025-10-09 10:20:19 +0200

    changes

--- Software/OLED_Clockswitch/src/main.cpp
+++ Software/OLED_Clockswitch/src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <U8g2lib.h>
+#include <Wire.h>
 
 #define ClockButtonSwitch 12
 
@@ -7,6 +8,12 @@
 #define LED2 27
 #define LED3 26
 
+
+#define SDA_PIN 13
+#define SCL_PIN 25
+
+
+U8G2_SSD1306_128X32_UNIVISION_F_SW_I2C u8g2(U8G2_R0, SDA_PIN, SCL_PIN, U8X8_PIN_NONE);
 
 
 int counter = 0;
@@ -18,8 +25,13 @@ void setup() {
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
   pinMode(LED3, OUTPUT);
-      
   Serial.begin(9600);
+
+  u8g2.begin();
+  u8g2.setFont(u8g2_font_ncenB08_tr);  
+
+ 
+
 }
 
 void ClockCount(int counter) {
@@ -28,22 +40,32 @@ void ClockCount(int counter) {
     digitalWrite(LED1, HIGH);
     digitalWrite(LED2, LOW);
     digitalWrite(LED3, LOW);
+
+
   }
   else if(counter % 3 == 1) {
     Serial.println("Selected Clock: " + ClockStates[1]);
     digitalWrite(LED1, LOW);
     digitalWrite(LED2, HIGH);
     digitalWrite(LED3, LOW);
+  
+
   }
   else if(counter % 3 == 2) {
+
     Serial.println("Selected Clock: " + ClockStates[2]);
     digitalWrite(LED1, LOW);
     digitalWrite(LED2, LOW);
     digitalWrite(LED3, HIGH);
+
   }
   else {
     Serial.println("ClockCount Error");
+   
   }
+
+ 
+
 }
 
 void loop() {
@@ -52,6 +74,11 @@ void loop() {
     counter++;
     while(digitalRead(ClockButtonSwitch) == HIGH);
   }
+
+ 
+  u8g2.clearBuffer();
+  u8g2.drawStr(0, 10, "Hallo OLED");
+  u8g2.sendBuffer();
   
 }
 
