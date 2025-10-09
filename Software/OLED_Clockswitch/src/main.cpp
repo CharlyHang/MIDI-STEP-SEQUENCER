10ab4dbdc3776c5cee7466456bbc82318fc8a950
mirnes114 <mirnes.husic@bulme.at>
2025-10-09 10:47:29 +0200

    Letzte Funktionierende Version vom Clockswitch --> ergänzen von oled mit u8g2

--- Software/OLED_Clockswitch/src/main.cpp
+++ Software/OLED_Clockswitch/src/main.cpp
@@ -1,31 +1,24 @@
 #include <Arduino.h>
-#include <U8g2lib.h>
-#include <Wire.h>
 
 #define ClockButtonSwitch 12
+
+
+//Einbau von OLED mit u8g2 fehlt, mit Adafruit hat funktioniert
 
 #define LED1 32
 #define LED2 27
 #define LED3 26
 
-
-
-
 int counter = 0;
 String ClockStates[3] = {"MIDI", "Intern", "Analog"}; //analog, intern, midi clocks
 
-
 void setup() {
+  //ClockButtonSwitchSettup
   pinMode(ClockButtonSwitch, INPUT);
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
   pinMode(LED3, OUTPUT);
   Serial.begin(9600);
-
-  
-
- 
-
 }
 
 void ClockCount(int counter) {
@@ -34,32 +27,22 @@ void ClockCount(int counter) {
     digitalWrite(LED1, HIGH);
     digitalWrite(LED2, LOW);
     digitalWrite(LED3, LOW);
-
-
   }
   else if(counter % 3 == 1) {
     Serial.println("Selected Clock: " + ClockStates[1]);
     digitalWrite(LED1, LOW);
     digitalWrite(LED2, HIGH);
     digitalWrite(LED3, LOW);
-  
-
   }
   else if(counter % 3 == 2) {
-
     Serial.println("Selected Clock: " + ClockStates[2]);
     digitalWrite(LED1, LOW);
     digitalWrite(LED2, LOW);
     digitalWrite(LED3, HIGH);
-
   }
   else {
     Serial.println("ClockCount Error");
-   
   }
-
- 
-
 }
 
 void loop() {
@@ -68,8 +51,5 @@ void loop() {
     counter++;
     while(digitalRead(ClockButtonSwitch) == HIGH);
   }
-
-  
-  
 }
 
