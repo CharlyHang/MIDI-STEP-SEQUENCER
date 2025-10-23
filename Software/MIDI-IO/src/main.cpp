58007d654fcb1ace3e8165ac37eeb4705f942e11
Charly Hang <Charly.hang@bulme.com>
2025-10-23 10:24:22 +0200

    Mehrere Noten in einem Step speichern funktioniert bis auf die letzte Note

--- Software/MIDI-IO/src/main.cpp
+++ Software/MIDI-IO/src/main.cpp
@@ -18,8 +18,9 @@ struct MidiMessage {
 MidiMessage msg[MAX_NOTE_SIZE][MAX_STEP_SIZE];
 int Note = 0;
 int Index  = 0;
-unsigned long start = 0, stop = 0;
-bool read = false;
+unsigned long start = 0;
+unsigned long stop = 0;
+bool first = false;
 
 void setup() {
   Serial.begin(9600);
@@ -42,40 +43,42 @@ void loop() {
 
   start = millis();
   stop = start;
-  read = true; // Um die erste Note noch zu lesen
-  while(stop - start <= 3000){
-    if(MIDI.read() || read && Note != MAX_NOTE_SIZE - 1){
-      read = false;
-      msg[Note][Index].type = MIDI.getType();
-      msg[Note][Index].channel = MIDI.getChannel();
-      msg[Note][Index].data1 = MIDI.getData1();
-      msg[Note][Index].data2 = MIDI.getData2();
+  first = true; // Um die erste Note noch zu lesen
+  while(stop - start <= 10000){
+     bool hasMessage = MIDI.read();  // <-- nur einmal pro Durchlauf
+  
+      if ((hasMessage || first) && Note != MAX_NOTE_SIZE - 1) {
+        first = false;
+        msg[Note][Index].type = MIDI.getType();
+        msg[Note][Index].channel = MIDI.getChannel();
+        msg[Note][Index].data1 = MIDI.getData1();
+        msg[Note][Index].data2 = MIDI.getData2();
 
-      Serial.print("Empfangen -> Typ: ");
-      Serial.print(msg[Note][Index].type);
-      Serial.print(" | Kanal: ");
-      Serial.print(msg[Note][Index].channel);
-      Serial.print(" | Data1: ");
-      Serial.print(msg[Note][Index].data1);
-      Serial.print(" | Data2: ");
-      Serial.println(msg[Note][Index].data2);
+        Serial.print("Empfangen -> Typ: ");
+        Serial.print(msg[Note][Index].type);
+        Serial.print(" | Kanal: ");
+        Serial.print(msg[Note][Index].channel);
+        Serial.print(" | Data1: ");
+        Serial.print(msg[Note][Index].data1);
+        Serial.print(" | Data2: ");
+        Serial.println(msg[Note][Index].data2);
 
-      switch (msg[Note][Index].type) {
-        case midi::NoteOn:
-          MIDI.sendNoteOn(msg[Note][Index].data1, msg[Note][Index].data2, msg[Note][Index].channel);
-          break;
-        case midi::NoteOff:
-          MIDI.sendNoteOff(msg[Note][Index].data1, msg[Note][Index].data2, msg[Note][Index].channel);
-          break;
-        default:
-          break;
+        switch (msg[Note][Index].type) {
+          case midi::NoteOn:
+            MIDI.sendNoteOn(msg[Note][Index].data1, msg[Note][Index].data2, msg[Note][Index].channel);
+            break;
+          case midi::NoteOff:
+            MIDI.sendNoteOff(msg[Note][Index].data1, msg[Note][Index].data2, msg[Note][Index].channel);
+            break;
+          default:
+            break;
+        }
+        Serial.println("Nachricht weitergeleitet");
+        Note = (Note == MAX_NOTE_SIZE - 1) ? 0 : Note + 1;
+      } else if (hasMessage && Note == MAX_NOTE_SIZE - 1){
+        Serial.println("Die maximale Notenanzahl fuer einen Step wurde erreicht!");
       }
-      Serial.println("Nachricht weitergeleitet");
-      Note = (Note == MAX_NOTE_SIZE - 1) ? 0 : Note + 1;
       stop = millis();
-    } else if (MIDI.read() || read && Note != MAX_NOTE_SIZE - 1){
-      Serial.println("Die maximale Notenanzahl fuer einen Step wurde erreicht!");
-    }
   }
   Note = 0;
   Index = (Index == MAX_STEP_SIZE - 1) ? 0 : Index + 1;
